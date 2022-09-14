d50c874267863b8b6b06a9b39507c03ee9dea60d
Rostislav Fedoriv <froizerner@gmail.com>
2022-09-14 20:48:57 +0300

    aaded data seeder

---
+++ Nix_group_project/DataAccess/Data.cs
@@ -0,0 +1,37 @@
+﻿using Core.Entites;
+
+namespace DataAccess;
+public class Data
+{
+    private static Data? _data;
+    public static Data Instance()
+    {
+        if (_data is null)
+        {
+            _data = new();
+        }
+        return _data;
+    }
+    private Data()
+    {
+
+    }
+    public List<Person> People { get; set; } = new()
+    {
+
+        new Person()
+        {
+            Id = 1,
+            FirstName ="Max",
+            LastName ="Peterson",
+            BirthDate = DateTime.Now,
+        },
+         new Person()
+        {
+            Id = 2,
+            FirstName ="Goofy",
+            LastName ="Bark",
+            BirthDate = DateTime.Now.AddDays(-1),
+        }
+    };
+}
