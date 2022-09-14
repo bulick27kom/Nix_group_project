5217a76c1c84daf1422e7b1c0f449cb8f53123c8
Rostislav Fedoriv <froizerner@gmail.com>
2022-09-14 20:47:26 +0300

    Fixed Entites

--- Nix_group_project/Core/Entites/Person.cs
+++ Nix_group_project/Core/Entites/Person.cs
@@ -1,6 +1,7 @@
 ﻿namespace Core.Entites;
 public sealed class Person
 {
+    public int? Id { get; set; } 
     public string? FirstName { get; set; }
     public string? LastName { get; set; }
     public DateTime BirthDate { get; set; }
