ef20ed8848a669bf5c9aea933041e53be606afe0
Rostislav Fedoriv <froizerner@gmail.com>
2022-09-14 14:41:20 +0300

    Added intrefaces

---
+++ Nix_group_project/Core/Interfaces/IPersonRepository.cs
@@ -0,0 +1,11 @@
+﻿using Core.Entites;
+
+namespace Core.Interfaces;
+public interface IPersonRepository
+{
+    public void Get();
+    public void Get(int id);
+    public void Create(Article article);
+    public void Update(Article article);
+    public void Delete(int id);
+}
