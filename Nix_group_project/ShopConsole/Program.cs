da9050ee36d5c886432f6a76f15350f70140e735
Rostislav Fedoriv <froizerner@gmail.com>
2022-09-14 20:49:07 +0300

    Added controller

--- Nix_group_project/ShopConsole/Program.cs
+++ Nix_group_project/ShopConsole/Program.cs
@@ -1,10 +1,22 @@
-﻿using Core;
+﻿
+using Core.Entites;
+using WebApi.Controllers;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
-
+        PersonController personController = new PersonController();
+        personController.Get();
+        personController.Get(1);
+        personController.Create(new Person()
+        {
+            Id=3,
+            FirstName="test1"
+        });
+        personController.Delete(1);
+        personController.Get();
+        personController.Update(new Person());
     }
 
 }
---
+++ Nix_group_project/WebApi/Controllers/PersonController.cs
@@ -0,0 +1,44 @@
+﻿using Core.Entites;
+using Core.Interfaces;
+using DataAccess.Repositories;
+
+namespace WebApi.Controllers;
+public class PersonController
+{
+    private readonly IPersonRepository _personRepository;
+
+    public PersonController()
+    {
+        _personRepository =(IPersonRepository) new PersonRepository();
+    }
+
+    public void Get()
+    {
+       var people = _personRepository.Get();
+        foreach (var person in people)
+        {
+            Console.WriteLine($"Id: {person.Id}, Name: {person.FirstName} {person.LastName}");
+        }
+    }
+
+    public void Get(int id)
+    { 
+        var person = _personRepository.Get(id);
+        Console.WriteLine($"Id: {person.Id}, Name: {person.FirstName} {person.LastName}");
+    }
+
+    public void Delete(int id)
+    {
+        var person = _personRepository.Get(id);
+        _personRepository.Delete(id);
+        Console.WriteLine($"Id: {person.Id}, Name: {person.FirstName} {person.LastName}");
+    }
+    public void Update(Person person)
+    {
+        _personRepository.Update(person);
+    }
+    public void Create(Person person)
+    {
+        _personRepository.Create(person);
+    }
+}
