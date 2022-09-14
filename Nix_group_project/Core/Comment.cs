3c978dbe52208e251364f9fb77cd047fa8e23093
Roman Brauer <bulick27kom@gmail.com>
2022-09-14 09:16:37 +0300

    Add Comment.cs class for working with comments.

---
+++ Nix_group_project/Core/Comment.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+
+    internal class Comment : ILiker
+    {
+        #region Fields
+        private List<Like> _likes;
+        private List<Like> _dislikes;
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// Коментируемый продукт
+        /// </summary>
+        public Product Product { get; set; }
+        
+        /// <summary>
+        /// Текст коментария 
+        /// </summary>
+        public string Text { get; set; } = "";
+
+        /// <summary>
+        /// Дата создания коментария
+        /// </summary>
+        public DateTime CreatData { get; set; }
+
+        /// <summary>
+        /// Пользователь, который оставил коментарий
+        /// </summary>
+        public User User { get; private set; }
+
+        /// <summary>
+        /// Количество лайков у этого коментария
+        /// </summary>
+        public int Likes { get; private set; } = 0;
+
+        /// <summary>
+        /// Количество диздайков у этого коментария
+        /// </summary>
+        public int Dislikes { get; private set; } = 0;
+        #endregion
+
+       
+
+        public Comment(Product product, string text, User user)
+        {
+            _likes = new List<Like>();
+            _dislikes = new List<Like>();
+            Product = product;
+            Text = text;
+            CreatData = DateTime.Now;
+            User = user;
+        }
+
+        public bool AddLike(User user)
+        {
+            Like like = new Like(user);
+            if (_likes.Contains(like))
+                return false;
+            else 
+            {
+                _likes.Add(like);
+                return true;
+            }
+        }
+
+        public bool AddDisike(User user)
+        {
+            Like dislike = new Like(user);
+            if (_dislikes.Contains(dislike))
+                return false;
+            else
+            {
+                _dislikes.Add(dislike);
+                return true;
+            }
+        }
+
+        public bool AddDislike(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int CounLikes()
+        {
+            return _likes.Count;
+        }
+
+        public int CounDisikes()
+        {
+            return _dislikes.Count;
+        }
+    }
+
+    class Like 
+    {
+        public User User { get; private set; }
+        public Like(User user)
+        {
+            User = user;
+        }
+    }
+
+    interface ILiker
+    {
+        bool AddLike(User user);
+        bool AddDislike(User user);
+
+        int CounLikes();
+        int CounDisikes();
+    }
+
+
+    // Заглушки для не существующих типов
+    class User { }
+    class Product { }
+
+}
