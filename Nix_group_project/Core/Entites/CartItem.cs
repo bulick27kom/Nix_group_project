247a56746271778e3b03f6756b39f3465cda44f7
Fess696 <75900532+Fess696@users.noreply.github.com>
2022-09-21 02:52:55 +0300

    All information at readme
    Co-Authored-By: Oleksandr Vdovitchenko <kentsanya91@gmail.com>

---
+++ Nix_group_project/Core/Entites/CartItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Core.Entites
+{
+    public class CartItem
+    {
+        public string ItemId { get; set; }
+
+        public string CartId { get; set; }
+
+        public int Quantity { get; set; }
+
+        public System.DateTime DateCreated { get; set; }
+
+        public int ProductId { get; set; }
+
+        public virtual Product Product { get; set; }
+
+    }
+}
+
--- Nix_group_project/Core/Entites/Product.cs
+++ Nix_group_project/Core/Entites/Product.cs
@@ -2,4 +2,17 @@
 
 public class Product
 {
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public string Description { get; set; }
+
+    public string Factory { get; set; }
+
+    public string CategoryID { get; set; }
+
+    public decimal Price { get; set; }
+
+    //public string ImagePath { get; set;} = "noimage.png";
 }
