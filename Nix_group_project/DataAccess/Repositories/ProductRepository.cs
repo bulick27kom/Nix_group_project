172c800ba274197294c1b9e1b10fdda65ab6a805
Fess696 <nikita.gulidin@gmail.com>
2022-09-21 14:51:20 +0300

    Updated v1.01
    Updated v1.01

---
+++ Nix_group_project/DataAccess/Repositories/ProductRepository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Core;
+using Core.Interfaces;
+
+namespace DataAccess.Repositories
+{
+    public class ProductRepository : IProductRepository
+    { 
+            private readonly ProductsEntities db = new ProductsEntities();
+
+            public void Add(Product pr)
+            {
+                db.Products.Add(pr);
+                db.SaveChanges();
+            }
+
+            public void Edit(Product pr)
+            {
+                db.Entry(pr).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+            }
+
+            public Product FindById(int id)
+            {
+                var result = (from pr in db.Products
+                              join c in db.Categories on pr.CategoryID equals c.CategoryID
+                              where pr.ProductID == id
+                              select pr.FirstOrDefault();
+
+                return result;
+            }
+
+            public IEnumerable<Product> GetProducts()
+            {
+                return db.Products.Include(c => c.Category);
+            }
+
+            public void Remove(int id)
+            {
+                Product pr = db.Products.Find(id);
+                db.Products.Remove(pr);
+                db.SaveChanges();
+            }
+        }
+    }
+}
+
