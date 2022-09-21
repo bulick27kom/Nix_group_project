b6a32008eb6884e939da19c745a5d9d2d4fcda88
Rostislav Fedoriv <froizerner@gmail.com>
2022-09-21 18:08:13 +0300

    created article controller

---
+++ Nix_group_project/WebApi/Controllers/ArticleController.cs
@@ -0,0 +1,42 @@
+﻿using Core.Entites;
+using Core.Interfaces;
+using DataAccess.Repositories;
+
+namespace WebApi.Controllers;
+public class ArticleController
+{
+    private readonly IArticleRepository _articleRepository;
+    public ArticleController()
+    {
+        _articleRepository = (IArticleRepository)new ArticleController();
+    }
+    public void Get()
+    {
+        var articles = _articleRepository.Get();
+        foreach (var article in articles)
+        {
+            Console.WriteLine($"Id: {article.Id}, Name: {article.Title} {article.Text}");
+        }
+    }
+
+    public void Get(int id)
+    {
+        var article = _articleRepository.Get(id);
+        Console.WriteLine($"Id: {article.Id}, Name: {article.Title} {article.Text}");
+    }
+
+    public void Delete(int id)
+    {
+        var article = _articleRepository.Get(id);
+        _articleRepository.Delete(id);
+        Console.WriteLine($"Id: {article.Id}, Name: {article.Title} {article.Text}");
+    }
+    public void Update(Article article)
+    {
+        _articleRepository.Update(article);
+    }
+    public void Create(Article article)
+    {
+        _articleRepository.Create(article);
+    }
+}
