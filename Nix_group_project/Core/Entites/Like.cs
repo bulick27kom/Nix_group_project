c36fc42b1a5a852ea0c91394eca3fe5f341e4da8
Roman Brauer <bulick27kom@gmail.com>
2022-10-12 09:37:50 +0300

    Removed likes functionality from Core

---
+++ Nix_group_project/Core/Entites/Like.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entites
+{
+    public class Like
+    {
+        int ID { get; set; }
+        public int LikesCount { get; set; } 
+
+        public int DislikesCount { get; set; }
+
+        public List<Person> UsersLikes { get; set; }
+    }
+}
