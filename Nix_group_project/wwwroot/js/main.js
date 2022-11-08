e9f03e3b09450bcd57cd1f75fe084621f6778580
Roman Brauer <bulick27kom@gmail.com>
2022-11-08 19:49:23 +0200

    Add html pemplate of main page

---
+++ Nix_group_project/wwwroot/js/main.js
@@ -0,0 +1,14 @@
+$(function() {
+
+	$('.open-search').click(function(e) {
+		e.preventDefault();
+		$('#search').addClass('active');
+	});
+
+	$('.close-search').click(function() {
+		$('#search').removeClass('active');
+	});
+
+
+
+});
